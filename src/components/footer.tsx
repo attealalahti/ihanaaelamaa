3e80614524080ba88504b016f63082ae71f11092
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2023-01-25 21:35:18 +0200

    Modify footer links to open in a new tab.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,18 +8,24 @@ const Footer: React.FC = () => {
       <span className="flex-1" />
       <a
         href="mailto:burleskia@ihanaaelamaa.fi"
+        target="_blank"
+        rel="noreferrer"
         className="mr-2 mt-1 flex justify-center p-2 align-middle hover:underline"
       >
         burleskia@ihanaaelamaa.fi
       </a>
       <a
         href="https://www.facebook.com/ihanaaelamaa/"
+        target="_blank"
+        rel="noreferrer"
         className="mr-2 mb-2 p-2"
       >
         <Image width={30} height={30} src={facebookIcon} alt="Facebook" />
       </a>
       <a
         href="https://www.instagram.com/ihanaaelamaayhdistys/"
+        target="_blank"
+        rel="noreferrer"
         className="mr-4 mb-2 p-2"
       >
         <Image width={30} height={30} src={instagramIcon} alt="Instagram" />
