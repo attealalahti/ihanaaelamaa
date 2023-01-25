37dab10e3886a1aa9d683b313f5dc6a5aef49f95
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2023-01-25 19:28:08 +0200

    Add social links to footer.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,29 @@
+import Image from "next/image";
+import facebookIcon from "../../public/images/facebook_icon.svg";
+import instagramIcon from "../../public/images/instagram_icon.svg";
+
 const Footer: React.FC = () => {
   return (
     <footer className="flex w-full text-white">
       <span className="flex-1" />
-      <span className="mr-4 p-2">test</span>
+      <a
+        href="mailto:burleskia@ihanaaelamaa.fi"
+        className="mr-2 mt-1 flex justify-center p-2 align-middle hover:underline"
+      >
+        burleskia@ihanaaelamaa.fi
+      </a>
+      <a
+        href="https://www.facebook.com/ihanaaelamaa/"
+        className="mr-2 mb-2 p-2"
+      >
+        <Image width={30} height={30} src={facebookIcon} alt="Facebook" />
+      </a>
+      <a
+        href="https://www.instagram.com/ihanaaelamaayhdistys/"
+        className="mr-4 mb-2 p-2"
+      >
+        <Image width={30} height={30} src={instagramIcon} alt="Instagram" />
+      </a>
     </footer>
   );
 };
