9cbe6c02e6ea0a8138dd088e2f19115b059dedab
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2023-01-26 22:58:28 +0200

    Add protected route to get all events.

--- src/server/trpc/router/event.ts
+++ src/server/trpc/router/event.ts
@@ -1,7 +1,10 @@
-import { router, publicProcedure } from "../trpc";
+import { router, publicProcedure, protectedProcedure } from "../trpc";
 
 export const eventRouter = router({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.posts.findMany();
+  }),
+  getAllProtected: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.event.findMany();
   }),
 });
