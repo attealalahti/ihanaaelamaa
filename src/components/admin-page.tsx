94be1eee8d17ea31baa3733f44122de8108a0b12
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2023-01-26 20:57:41 +0200

    Add AdminPage as a separate component.

---
+++ src/components/admin-page.tsx
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import Page from "./page";
+import AdminHeader from "./admin-header";
+import SignedOut from "./signed-out";
+import Unauthorized from "./unauthorized";
+import { type Session } from "next-auth";
+
+type Props = {
+  children: React.ReactNode;
+  session: Session | null;
+};
+
+const AdminPage: React.FC<Props> = ({ children, session }) => {
+  return (
+    <>
+      <Head>
+        <title>Ihanaa Elämää ry - Admin</title>
+        <meta name="description" content="Burleskiyhdistys" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Page admin>
+        <AdminHeader session={session} />
+        <main className="flex flex-1 flex-col items-center justify-center">
+          {!session ? (
+            <SignedOut />
+          ) : !session.user?.isAdmin ? (
+            <Unauthorized session={session} />
+          ) : (
+            children
+          )}
+        </main>
+      </Page>
+    </>
+  );
+};
+
+export default AdminPage;
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,40 +1,20 @@
 import { type NextPage } from "next";
-import Head from "next/head";
-import Page from "../../components/page";
 import { useSession } from "next-auth/react";
-import Unauthorized from "../../components/unauthorized";
-import SignedOut from "../../components/signed-out";
-import AdminHeader from "../../components/admin-header";
 import Link from "next/link";
+import AdminPage from "../../components/admin-page";
 
 const AdminHome: NextPage = () => {
   const { data: session } = useSession();
 
   return (
-    <>
-      <Head>
-        <title>Ihanaa Elämää ry - Admin</title>
-        <meta name="description" content="Burleskiyhdistys" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Page admin>
-        <AdminHeader session={session} />
-        <main className="flex flex-1 flex-col items-center justify-center">
-          {!session ? (
-            <SignedOut />
-          ) : !session.user?.isAdmin ? (
-            <Unauthorized session={session} />
-          ) : (
-            <Link
-              href="/admin/events"
-              className="rounded-xl bg-white p-4 text-2xl text-black"
-            >
-              Tapahtumat
-            </Link>
-          )}
-        </main>
-      </Page>
-    </>
+    <AdminPage session={session}>
+      <Link
+        href="/admin/events"
+        className="rounded-xl bg-white p-4 text-2xl text-black"
+      >
+        Tapahtumat
+      </Link>
+    </AdminPage>
   );
 };
 
