42be07e99459ac13fa419c77d20a59c9dcaa7b60
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2023-01-28 13:21:27 +0200

    Modify /admin/events to display events in a table.

--- src/pages/admin/events/index.tsx
+++ src/pages/admin/events/index.tsx
@@ -3,7 +3,12 @@ import { useSession } from "next-auth/react";
 import AdminPage from "../../../components/admin-page";
 import { trpc } from "../../../utils/trpc";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight, faPlus } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTrashCan,
+  faPlus,
+  faEye,
+  faEyeSlash,
+} from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 
 const AdminEvents: NextPage = () => {
@@ -16,39 +21,44 @@ const AdminEvents: NextPage = () => {
         <section className="flex max-w-4xl flex-col items-center justify-center text-white">
           <Link
             href="/admin/events/new"
-            className="m-6 rounded-xl bg-white p-4 text-xl font-bold text-black hover:bg-slate-200"
+            className="m-4 rounded-xl bg-white p-4 text-xl font-bold text-black hover:bg-slate-200"
           >
             Luo uusi tapahtuma
             <span className="ml-2">
               <FontAwesomeIcon icon={faPlus} size="lg" />
             </span>
           </Link>
-          {events.data.map(({ id, title, description, date }, index) => (
-            <Link
-              key={index}
-              href={`/admin/events/${id}`}
-              className={`${
-                index === events.data.length - 1
-                  ? "rounded-b-xl"
-                  : " border-b border-slate-300"
-              } ${
-                index === 0 ? "rounded-t-xl" : ""
-              } group grid w-full grid-flow-col bg-white px-4 py-3 text-left text-black hover:bg-slate-200`}
-            >
-              <div>
-                <p className="text-sm">{date.toLocaleDateString()}</p>
-                <h2 className="text-lg font-bold group-hover:underline lg:text-xl">
-                  {title}
-                </h2>
-                <p>{description}</p>
+          <div className="w-screen max-w-4xl p-4">
+            {events.data.map(({ id, title, description, date }, index) => (
+              <div
+                key={id}
+                className={`group grid grid-flow-col grid-cols-1 gap-1 bg-white text-lg text-black hover:bg-slate-200 ${
+                  index === 0
+                    ? "rounded-t-lg"
+                    : index === events.data.length - 1
+                    ? "rounded-b-lg border-t border-slate-400"
+                    : "border-t border-slate-400"
+                }`}
+              >
+                <Link
+                  href={`/admin/events/${id}`}
+                  className="grid grid-flow-col grid-cols-8 gap-3 p-2"
+                >
+                  <div className="col-span-2 font-bold group-hover:underline">
+                    {title}
+                  </div>
+                  <div className="col-span-5">{description}</div>
+                  <div className="col-span-1">{date.toLocaleDateString()}</div>
+                </Link>
+                <button className="p-2 opacity-75 transition-all hover:scale-110 hover:opacity-100">
+                  <FontAwesomeIcon icon={faEye} size="lg" />
+                </button>
+                <button className="mr-2 p-2 opacity-75 transition-all hover:scale-110 hover:opacity-100">
+                  <FontAwesomeIcon icon={faTrashCan} size="lg" />
+                </button>
               </div>
-              <div className="flex h-full justify-end">
-                <span className="mt-auto p-1 pl-4 opacity-75 transition-all duration-300 group-hover:scale-[1.3] group-hover:opacity-100">
-                  <FontAwesomeIcon icon={faArrowRight} size="2x" />
-                </span>
-              </div>
-            </Link>
-          ))}
+            ))}
+          </div>
         </section>
       ) : events.isError ? (
         <div className="text-white">Error</div>
