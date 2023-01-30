0803500129b742f0ed202d97991437c2295fa844
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2023-01-30 12:41:34 +0200

    Add notification to admin header for unpublished changes.

--- src/components/admin-header.tsx
+++ src/components/admin-header.tsx
@@ -2,6 +2,9 @@ import { type Session } from "next-auth";
 import Link from "next/link";
 import { useState } from "react";
 import { signOut } from "next-auth/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWarning } from "@fortawesome/free-solid-svg-icons";
+import { trpc } from "../utils/trpc";
 
 const HAMBURGER_PATH =
   "M4,4 L27,4 C28,4 28,7 27,7 L4,7 C3,7 3,4 4,4 M4,14 L27,14 C28,14 28,17 27,17 L4,17 C3,17 3,14 4,14 M27,27 L4,27 C3,27 3,24 4,24 L27,24 C28,24 28,27 27,27";
@@ -14,6 +17,8 @@ type Props = {
 
 const AdminHeader: React.FC<Props> = ({ session }) => {
   const [navOpen, setNavOpen] = useState(false);
+
+  const unpublishedChanges = trpc.auth.unpublishedChanges.useQuery();
 
   if (!session) return null;
 
@@ -45,12 +50,28 @@ const AdminHeader: React.FC<Props> = ({ session }) => {
       </div>
       <nav className={`${!navOpen ? "hidden md:block" : ""} md:mr-4 lg:mr-12`}>
         <ul className="h-full w-full text-lg md:flex md:flex-row md:gap-6">
+          {unpublishedChanges.data !== undefined && unpublishedChanges.data && (
+            <li className="group relative flex md:items-center md:justify-center">
+              <span className="absolute top-full hidden rounded border border-slate-300 bg-white p-1 text-center text-black group-hover:inline">
+                Muista julkaista muutokset, kun olet valmis
+              </span>
+              <Link
+                href="/admin"
+                className="w-screen rounded-3xl bg-white p-4 pl-6 text-black hover:bg-slate-200 md:w-auto md:pl-4"
+              >
+                Julkaisemattomia muutoksia
+                <span className="ml-2">
+                  <FontAwesomeIcon icon={faWarning} />
+                </span>
+              </Link>
+            </li>
+          )}
           <li className="flex md:items-center md:justify-center">
             <span className="w-screen p-2 md:w-auto">{session.user?.name}</span>
           </li>
           <li className="flex md:items-center md:justify-center">
             <button
-              className="w-screen rounded-xl bg-white p-2 text-black md:w-auto"
+              className="w-screen rounded-xl bg-white p-2 text-black hover:bg-slate-200 md:w-auto"
               onClick={() => signOut()}
             >
               Kirjaudu ulos
--- src/server/trpc/router/auth.ts
+++ src/server/trpc/router/auth.ts
@@ -1,9 +1,21 @@
 import { router, protectedProcedure } from "../trpc";
 import axios from "axios";
 import { env } from "../../../env/server.mjs";
+import { TRPCError } from "@trpc/server";
 
 export const authRouter = router({
-  build: protectedProcedure.mutation(async () => {
+  build: protectedProcedure.mutation(async ({ ctx }) => {
     await axios.post(env.BUILD_HOOK_URL, {});
+    await ctx.prisma.unpublishedChanges.update({
+      where: { id: 1 },
+      data: { value: false },
+    });
+  }),
+  unpublishedChanges: protectedProcedure.query(async ({ ctx }) => {
+    const changes = await ctx.prisma.unpublishedChanges.findFirst({
+      where: { id: 1 },
+    });
+    if (!changes) throw new TRPCError({ code: "INTERNAL_SERVER_ERROR" });
+    return changes.value;
   }),
 });
