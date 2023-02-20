dbece4c86ce16a96abea4f1475cc233fb2b81291
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2023-02-20 13:10:28 +0200

    Order images in image.all procedure by upload date.

--- src/server/trpc/router/image.ts
+++ src/server/trpc/router/image.ts
@@ -2,6 +2,6 @@ import { router, protectedProcedure } from "../trpc";
 
 export const imageRouter = router({
   all: protectedProcedure.query(({ ctx }) => {
-    return ctx.prisma.image.findMany();
+    return ctx.prisma.image.findMany({ orderBy: { created_at: "asc" } });
   }),
 });
