409334d1161a296de5b4315746d7e243ba0399f3
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2023-06-21 10:39:43 +0300

    Modify useDropshop to repeatedly search for the right element.

--- src/components/content/dropshop.tsx
+++
@@ -1,9 +0,0 @@
-import useDropshop from "../../hooks/use-dropshop";
-
-const Dropshop = () => {
-  useDropshop();
-
-  return null;
-};
-
-export default Dropshop;
--- src/hooks/use-dropshop.ts
+++ src/hooks/use-dropshop.ts
@@ -2,10 +2,19 @@ import { useEffect } from "react";
 
 const useDropshop = () => {
   useEffect(() => {
-    const dropshop = document.getElementById("dropshop-loader");
-    if (dropshop) {
+    let dropshop = document.getElementById("dropshop-loader");
+
+    const showDropshop = async () => {
+      const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+      while (!dropshop) {
+        await sleep(100);
+        dropshop = document.getElementById("dropshop-loader");
+      }
       dropshop.style.display = "block";
-    }
+    };
+    showDropshop();
+
     return () => {
       if (dropshop) {
         dropshop.style.display = "none";
